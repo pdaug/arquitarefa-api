ec9a5689f69e710f86a0bc9e407682b86fe48099
pdaug <dev.pdaug@gmail.com>
2023-05-16 20:26:31 -0300

    test: new routers

--- api/index.js
+++ api/index.js
@@ -1,7 +1,43 @@
-export default function(request, response) {
+import { MongoClient } from "mongodb";
 
-    const body = request.body;
-        
-    return response.status(200).json({ body, method: request.method });
+export default async function(request, response) {
+
+    if (request.method === "GET") {
+
+        const client = new MongoClient(process.env.MONGODB_URI);
+
+        await client.connect();
+
+        const database = client.db("teste")
+
+        const collection = database.collection("teste2");
+
+        const cursor = collection.find();
+
+        const allValues = await cursor.toArray();
+
+        return response.status(200).json(allValues);
+
+    }
+
+    else if (request.method === "POST") {
+
+        const body = request.body;
+
+        const client = new MongoClient(process.env.MONGODB_URI);
+
+        await client.connect();
+
+        const database = client.db("teste")
+
+        const collection = database.collection("teste2");
+
+        const result = await collection.insertOne(body);
+
+        return response.status(200).json(result);
+
+    }
+
+    return response.status(400);
 
 }
