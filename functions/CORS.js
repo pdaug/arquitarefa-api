71991cb316cca440b47cb70a19c1e874e044b5d7
pdaug <dev.pdaug@gmail.com>
2023-05-17 20:51:33 -0300

    feature: cors function

--- functions/CORS.js
+++ functions/CORS.js
@@ -6,9 +6,17 @@ export default function CORS(route) {
 
         response.setHeader("Access-Control-Allow-Origin", "*");
 
-        response.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, PATCH, DELETE, POST, PUT");
+        response.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
     
         response.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+
+        if (request.method === "OPTIONS") { 
+            
+            res.status(200).end();
+            
+            return;
+        
+        }
 
         return await route(request, response);
 
